8f8d20da0f428972b10c293eb70f096b6e81134f
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2025-06-26 15:45:46 +0200

    fix: use only one loop

--- python/6_sum_square_difference.py
+++ python/6_sum_square_difference.py
@@ -7,16 +7,18 @@ $$(1 + 2 + ... + 10)^2 = 55^2 = 3025.$$
 <p>Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.</p>
 """
 
-def sum_square_difference():
-    sum_of_squares = sum([n**2 for n in range(1, 101)])
+def sum_square_difference(limit):
+    sum_of_squares = 0
+    sum = 0
     
-    square_of_sum = sum([n for n in range(1,101)])
-    square_of_sum = square_of_sum**2
+    for n in range(1, limit+1):
+        sum_of_squares += n**2
+        sum += n
 
-    return square_of_sum - sum_of_squares
+    return sum**2 - sum_of_squares
 
 def main():
-    print(sum_square_difference())
+    print(sum_square_difference(100)) # 25_164_150
 
 if __name__ == '__main__':
     main()
